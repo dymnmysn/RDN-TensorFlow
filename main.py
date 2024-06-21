5db49d7f2c678d09ab872b1cb5a1dbd7c095722e
dymnmysn <76868298+dymnmysn@users.noreply.github.com>
2024-06-21 20:03:51 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,46 +1,47 @@
 import tensorflow as tf
 from model import RDN
+import argparse
 
-flags = tf.app.flags
-FLAGS = flags.FLAGS
-flags.DEFINE_boolean("is_train", True, "if the train")
-flags.DEFINE_boolean("matlab_bicubic", False, "using bicubic interpolation in matlab")
-flags.DEFINE_integer("image_size", 32, "the size of image input")
-flags.DEFINE_integer("c_dim", 3, "the size of channel")
-flags.DEFINE_integer("scale", 3, "the size of scale factor for preprocessing input image")
-flags.DEFINE_integer("stride", 16, "the size of stride")
-flags.DEFINE_integer("epoch", 50, "number of epoch")
-flags.DEFINE_integer("batch_size", 16, "the size of batch")
-flags.DEFINE_float("learning_rate", 1e-4 , "the learning rate")
-flags.DEFINE_float("lr_decay_steps", 10 , "steps of learning rate decay")
-flags.DEFINE_float("lr_decay_rate", 0.5 , "rate of learning rate decay")
-flags.DEFINE_boolean("is_eval", True, "if the evaluation")
-flags.DEFINE_string("test_img", "", "test_img")
-flags.DEFINE_string("checkpoint_dir", "checkpoint", "name of the checkpoint directory")
-flags.DEFINE_string("result_dir", "result", "name of the result directory")
-flags.DEFINE_string("train_set", "DIV2K_train_HR", "name of the train set")
-flags.DEFINE_string("test_set", "Set5", "name of the test set")
-flags.DEFINE_integer("D", 16, "D")
-flags.DEFINE_integer("C", 8, "C")
-flags.DEFINE_integer("G", 64, "G")
-flags.DEFINE_integer("G0", 64, "G0")
-flags.DEFINE_integer("kernel_size", 3, "the size of kernel")
+def parse_args():
+    parser = argparse.ArgumentParser(description="TensorFlow RDN")
+    parser.add_argument("--is_train", type=bool, default=True, help="if the train")
+    parser.add_argument("--matlab_bicubic", type=bool, default=False, help="using bicubic interpolation in matlab")
+    parser.add_argument("--image_size", type=int, default=32, help="the size of image input")
+    parser.add_argument("--c_dim", type=int, default=3, help="the size of channel")
+    parser.add_argument("--scale", type=int, default=3, help="the size of scale factor for preprocessing input image")
+    parser.add_argument("--stride", type=int, default=16, help="the size of stride")
+    parser.add_argument("--epoch", type=int, default=50, help="number of epoch")
+    parser.add_argument("--batch_size", type=int, default=16, help="the size of batch")
+    parser.add_argument("--learning_rate", type=float, default=1e-4, help="the learning rate")
+    parser.add_argument("--lr_decay_steps", type=int, default=10, help="steps of learning rate decay")
+    parser.add_argument("--lr_decay_rate", type=float, default=0.5, help="rate of learning rate decay")
+    parser.add_argument("--is_eval", type=bool, default=True, help="if the evaluation")
+    parser.add_argument("--test_img", type=str, default="", help="test_img")
+    parser.add_argument("--checkpoint_dir", type=str, default="checkpoint", help="name of the checkpoint directory")
+    parser.add_argument("--result_dir", type=str, default="result", help="name of the result directory")
+    parser.add_argument("--train_set", type=str, default="DIV2K_train_HR", help="name of the train set")
+    parser.add_argument("--test_set", type=str, default="Set5", help="name of the test set")
+    parser.add_argument("--D", type=int, default=16, help="D")
+    parser.add_argument("--C", type=int, default=8, help="C")
+    parser.add_argument("--G", type=int, default=64, help="G")
+    parser.add_argument("--G0", type=int, default=64, help="G0")
+    parser.add_argument("--kernel_size", type=int, default=3, help="the size of kernel")
+    return parser.parse_args()
 
-
-
-def main(_):
-    rdn = RDN(tf.Session(),
-              is_train = FLAGS.is_train,
-              is_eval = FLAGS.is_eval,
-              image_size = FLAGS.image_size,
-              c_dim = FLAGS.c_dim,
-              scale = FLAGS.scale,
-              batch_size = FLAGS.batch_size,
-              D = FLAGS.D,
-              C = FLAGS.C,
-              G = FLAGS.G,
-              G0 = FLAGS.G0,
-              kernel_size = FLAGS.kernel_size
+def main():
+    FLAGS = parse_args()
+    
+    rdn = RDN(is_train=FLAGS.is_train,
+              is_eval=FLAGS.is_eval,
+              image_size=FLAGS.image_size,
+              c_dim=FLAGS.c_dim,
+              scale=FLAGS.scale,
+              batch_size=FLAGS.batch_size,
+              D=FLAGS.D,
+              C=FLAGS.C,
+              G=FLAGS.G,
+              G0=FLAGS.G0,
+              kernel_size=FLAGS.kernel_size
               )
 
     if rdn.is_train:
@@ -51,5 +52,5 @@ def main(_):
         else:
             rdn.test(FLAGS)
 
-if __name__=='__main__':
-    tf.app.run()
+if __name__ == '__main__':
+    main()
